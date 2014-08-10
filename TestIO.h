f3478b803146f6e5a350974f7cc2d5d801fcf654
Tomas Hozza <thozza@gmail.com>
2014-08-10 19:28:22 +0200

    Adjust HW version to latest (1.1) and fix compilation issue
    In file included from TestIO.ino:3:0:
    TestIO.h:5:20: fatal error: config.h: No such file or directory
     #include "config.h"
                         ^
                         compilation terminated.
    
    Signed-off-by: Tomas Hozza <thozza@gmail.com>

--- TestIO.h
+++ TestIO.h
@@ -2,9 +2,9 @@
 #define TESTIO_H
 
 #include "globals.h"
-#include "config.h"
+#include "Config.h"
 
-#define HARDWAREVERSION 10  // 1.0 , version number needs to fit in byte (0~255) to be able to store it into config, this should only be changed by the manufacturer !
+#define HARDWAREVERSION 11  // 1.1 , version number needs to fit in byte (0~255) to be able to store it into config, this should only be changed by the manufacturer!
 
 int TestIO();
 
